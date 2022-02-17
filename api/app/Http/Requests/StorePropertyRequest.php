80f516e9f6f8ac7f5772ff773c1777e23d2e0a9e
Loan Besson <loan.besson@hotmail.fr>
2022-02-17 18:32:17 +0100

    Start property factory and seeder

--- api/app/Http/Requests/StorePropertyRequest.php
+++ api/app/Http/Requests/StorePropertyRequest.php
@@ -24,9 +24,43 @@ class StorePropertyRequest extends FormRequest
     public function rules()
     {
         return [
-            'title'             => 'required|string|min:5|max:50',
-            'description'       => 'required|string|min:5|max:255',
-            'construction_date' => 'required|date|before:now'
+            "user_id"               => 'required',
+            "title"                 => 'required|string|min:5|max:50',
+            "description"           => 'required|string|min:5|max:255',
+            "type"                  => 'required',
+            "category"              => 'required',
+            "area"                  => 'required',
+            "floor"                 => 'required',
+            "floors"                => 'required',
+            "rooms"                 => 'required',
+            "bedrooms"              => 'required',
+            "bathrooms"             => 'required',
+            "toilets"               => 'required',
+            "is_furnished"          => 'required',
+            "contains_storage"      => 'required',
+            "is_kitchen_separated"  => 'required',
+            "contains_dining_room"  => 'required',
+            "ground"                => 'required',
+            "heater"                => 'required',
+            "fireplace"             => 'required',
+            "elevator"              => 'required',
+            "external_storage"      => 'required',
+            "area_external_storage" => 'required',
+            "guarding"              => 'required',
+            "energy_consumption"    => 'required',
+            "gas_emissions"         => 'required',
+            "address"               => 'required',
+            "zip_code"              => 'required',
+            "city"                  => 'required',
+            "rentOrSale"            => 'required',
+            "price"                 => 'required',
+            "charges"               => 'required',
+            "guarentee"             => 'required',
+            "fees_price"            => 'required',
+            "inventory_price"       => 'required',
+            "published"             => 'required',
+            "image_path"            => 'required',
+            "construction_date"     => 'required|date|before:now'
         ];
     }
 }
---
+++ api/database/factories/PropertyFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Property>
+ */
+class PropertyFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->text(25),
+            'description' => $this->faker->text(150),
+            'price' => 250000
+        ];
+    }
+}
