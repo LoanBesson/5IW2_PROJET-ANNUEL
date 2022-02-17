fd074b82ad0cc106c5a090c79bf43b51cde4494e
Loan Besson <loan.besson@hotmail.fr>
2022-02-17 16:57:00 +0100

    Except routes on property routes

--- api/routes/api.php
+++ api/routes/api.php
@@ -20,5 +20,5 @@ Route::post('login', [PassportAuthController::class, 'login']);
 
 Route::middleware('auth:api')->group(function () {
     Route::get('get-user', [PassportAuthController::class, 'userInfo']);
-    Route::resource('property', PropertyController::class);
+    Route::resource('property', PropertyController::class)->except(['create', 'edit']);
 });
