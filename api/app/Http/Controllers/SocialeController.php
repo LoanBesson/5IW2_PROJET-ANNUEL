70af3cec1003a06e5253db0906180e4777feffb0
Fendly98 <f.belhomme.jouvet@gmail.com>
2022-02-18 13:10:25 +0100

    fixed

--- api/app/Http/Controllers/SocialeController.php
+++ api/app/Http/Controllers/SocialeController.php
@@ -10,8 +10,24 @@ use App\User;
 class SocialeController extends Controller
 {
 
+    protected $providers = [ "google", "github", "facebook" ];
 
+    # La vue pour les liens vers les providers
+    public function loginRegister () {
+    	return view("socialite.login-register");
+    }
 
+    # redirection vers le provider
+    public function redirect (Request $request) {
+
+        $provider = $request->provider;
+
+        // On vérifie si le provider est autorisé
+        if (in_array($provider, $this->providers)) {
+            return Socialite::driver($provider)->redirect(); // On redirige vers le provider
+        }
+        abort(404); // Si le provider n'est pas autorisé
+    }
 
     public function callback (Request $request) {
 
