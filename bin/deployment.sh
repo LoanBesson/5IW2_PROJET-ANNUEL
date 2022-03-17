a589166ab323d2c4311ec40a505dcad15dacbe76
Loan Besson <loan.besson@hotmail.fr>
2022-03-17 22:08:13 +0100

    Heroku modification

--- bin/deployment.sh
+++ bin/deployment.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 php artisan storage:link
 php artisan optimize
-php artisan passport:keys
-php artisan migrate --force
-php artisan db:seed --force
+php artisan passport:keys --force
+php artisan migrate:fresh --force --seed
