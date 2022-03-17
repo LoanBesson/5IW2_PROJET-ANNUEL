7e0e961c947c66ed22055d5dc87300a92fc44807
Loan Besson <loan.besson@hotmail.fr>
2022-03-17 22:21:55 +0100

    composer heroku

--- bin/deployment.sh
+++ bin/deployment.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 composer install --optimize-autoloader --no-dev
+php artisan config:cache
+php artisan route:cache
 php artisan storage:link
 php artisan optimize
 php artisan passport:keys --force
