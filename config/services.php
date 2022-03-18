af1f21177c4dc20911cc5149ec6b033d54e030b0
Fendly98 <f.belhomme.jouvet@gmail.com>
2022-03-18 09:06:25 +0100

    fixed

--- config/services.php
+++ config/services.php
@@ -40,12 +40,6 @@ return [
         'client_id' => env('FACEBOOK_CLIENT_ID'),
         'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
         'redirect' => env('FACEBOOK_CLIENT_CALLBACK')
-    ],
-
-    'github' => [
-        'client_id' => env('GITHUB_CLIENT_ID'),
-        'client_secret' => env('GITHUB_CLIENT_SECRET'),
-        'redirect' => env('GITHUB_CLIENT_CALLBACK')
     ]
 
 
