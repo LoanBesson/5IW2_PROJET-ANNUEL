824f08ddc26fe3e854f82ffb91939af1da4facf0
Gueguen Dorian <doriangueguen@orange.fr>
2022-07-06 13:43:01 +0200

    filters

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -15,7 +15,7 @@ class PropertyController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth:api');
+        $this->middleware('auth:api')->except('index', 'show');
     }
 
     /**
