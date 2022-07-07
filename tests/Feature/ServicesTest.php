333cb0b36eed468ea1622315a1e0aacfff777c68
Fendly98 <f.belhomme.jouvet@gmail.com>
2022-07-08 00:35:02 +0200

    test

---
+++ tests/Feature/ServicesTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+
+use Tests\TestCase;
+
+
+class ServicesTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
